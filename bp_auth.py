9f4bff6558b55e780bf8597854176fad9019d680
Joseph Vigil <joeyvigil109329@gmail.com>
2025-08-09 12:14:29 -0600

    Cloned class repo

---
+++ bp_auth.py
@@ -0,0 +1,19 @@
+from models import Owners, session #Need the Users model to create and search for users
+#need the sesssion to add users to our db
+
+
+
+#Create Login function
+#get email and password from user
+#check database for owner with the given email
+#if you find an owner, check if the found owners password is the same as the given password
+#if so return user
+
+
+#Create Register function
+#get all info required to create an owner from the user
+#try and create an Owner from the info (will fail if email is already in user)
+#if you succeed return user
+#except error and print message
+
+
